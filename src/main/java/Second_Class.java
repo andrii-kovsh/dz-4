26086112c0c6a62bee12766e29e7d7956898ee67
Andrii Kovsh <kobannn@gmail.com>
2023-06-14 09:05:53 +0300

    Adding full code with completed task

--- src/main/java/Second_Class.java
+++ src/main/java/Second_Class.java
@@ -1,2 +1,8 @@
 public class Second_Class {
+    int field = 312; // Поле типу int з заданим значенням
+
+    public int getField() {
+        System.out.println("---" + field + "---"); // Друк значення поля у консоль
+        return field; // Повернення значення поля
+    }
 }
